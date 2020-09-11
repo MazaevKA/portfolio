d463bda632ec0241e3e7c02a2582b56dade07ea3
MazaevKA <mazaevka.ru@gmail.com>
2020-09-11 15:07:33 +0500

    Finish

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,110 +1,124 @@
-// $(document).ready(function () {
-//     const hamburger = document.querySelector('.hamburger');
-//     const mobNav = document.querySelector('.header__nav');
-//     const overlay = document.querySelector('#overlay');
-//     const noScroll = document.body;
+$(document).ready(function () {
 
-//     hamburger.addEventListener('click', function () {
 
-//         this.classList.toggle("is-active");
-//         mobNav.classList.toggle("is-active");
-//         overlay.classList.toggle("is-active");
-//         noScroll.classList.toggle("noscroll");
-//     });
+    // Мобильное меню
+    const hamburger = document.querySelector('.hamburger');
+    const mobNav = document.querySelector('.mobail__nav');
+    const overlay = document.querySelector('#overlay');
+    const noScroll = document.body;
 
-//     mobNav.addEventListener('click', function() {
-//         this.classList.remove('is-active');
-//         overlay.classList.remove("is-active");
-//         noScroll.classList.remove("noscroll");
-//         hamburger.classList.remove("is-active");
-//     })
-// });
+    hamburger.addEventListener('click', function () {
 
-// Form placeholder
-
-const formInputs = document.querySelectorAll('.form__field');
-for (let item of formInputs) {
-    const thisPlaceholder = item.nextElementSibling;
-    console.log(thisPlaceholder);
-    item.addEventListener('focus', function(){
-        thisPlaceholder.classList.add('active');
+        this.classList.toggle("active");
+        mobNav.classList.toggle("active");
+        overlay.classList.toggle("active");
+        noScroll.classList.toggle("noscroll");
+        console.log(mobNav);
     });
-    item.addEventListener('blur', function(){
-        if(item.value == ''){
-            thisPlaceholder.classList.remove('active');
+
+    mobNav.addEventListener('click', function() {
+
+        this.classList.remove('active');
+        overlay.classList.remove("active");
+        noScroll.classList.remove("noscroll");
+        hamburger.classList.remove("active");
+    });
+
+    // Закрытие мобильного меню при ресайзе экрана
+    window.addEventListener('resize', function(){
+        mobNav.classList.remove('active');
+        overlay.classList.remove("active");
+        noScroll.classList.remove("noscroll");
+        hamburger.classList.remove("active");
+    })
+
+    //Placeholder формы
+    const formInputs = document.querySelectorAll('.form__field');
+    for (let item of formInputs) {
+        const thisPlaceholder = item.nextElementSibling;
+        item.addEventListener('focus', function(){
+            thisPlaceholder.classList.add('active');
+        });
+        item.addEventListener('blur', function(){
+            if(item.value == ''){
+                thisPlaceholder.classList.remove('active');
+            }
+        })
+    }
+
+    //Фильтр блока портфолио
+    let containerEl = document.querySelector('#portfolio-project');
+    let mixer = mixitup(containerEl, {
+        classNames: {
+            block: ""
         }
     })
-}
 
-//Filter
-
-let containerEl = document.querySelector('#portfolio-project');
-let mixer = mixitup(containerEl, {
-    classNames: {
-        block: ""
-    }
-})
-//FORM VALIDATE
-$('#contact-form').validate({
-    rules: {
-        email: {
-            required: true,
-            email: true
+    //Валидация формы
+    $('#contact-form').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            topic: {
+                required: true
+            },
+            message: {
+                required: true
+            }
         },
-        topic: {
-            required: true
+        messages: {
+            email: {
+                required: 'Введите email',
+                email: 'отсутсвует символ @'
+            },
+            theme: {
+                required: 'Введите тему сообщения'
+            },
+            message: {
+                required: 'Введите текст сообщения'
+            }
         },
-        message: {
-            required: true
+        submitHandler: function (form) {
+            ajaxFormSubmit();
         }
-    },
-    messages: {
-        email: {
-            required: 'Введите email',
-            email: 'отсутсвует символ @'
-        },
-        theme: {
-            required: 'Введите тему сообщения'
-        },
-        message: {
-            required: 'Введите текст сообщения'
-        }
-    },
-    submitHandler: function (form) {
-        ajaxFormSubmit();
-    }
-})
-// Подключение точек пагинации справа page-nav
-$('#page-nav').onePageNav({
-    currentClass: 'active',
-    changeHash: false,
-    scrollSpeed: 750,
-    scrollThreshold: 0.5,
-    filter: '',
-    easing: 'swing',
-    begin: function () {},
-    end: function () {},
-    scrollChange: function ($currentListItem) {}
-});
-// Функция AJAX запрса на сервер
+    })
 
-function ajaxFormSubmit() {
-
-    let string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку.
-
-    //Формируем ajax запрос
-    $.ajax({
-        type: "POST", // Тип запроса - POST
-        url: "php/mail.php", // Куда отправляем запрос
-        data: string, // Какие даные отправляем, в данном случае отправляем переменную string
-
-        // Функция если все прошло успешно
-        success: function (html) {
-            $("#contact-form").slideUp(800);
-            $('#answer').html(html);
-        }
+    // Подключение точек пагинации справа page-nav
+    $('#page-nav').onePageNav({
+        currentClass: 'active',
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'swing',
+        begin: function () {},
+        end: function () {},
+        scrollChange: function ($currentListItem) {}
     });
 
-    // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
-    return false;
-}
+    // Функция AJAX запрса на сервер
+    function ajaxFormSubmit() {
+
+        let string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку.
+
+        //Формируем ajax запрос
+        $.ajax({
+            type: "POST", // Тип запроса - POST
+            url: "php/mail.php", // Куда отправляем запрос
+            data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+            // Функция если все прошло успешно
+            success: function (html) {
+                $("#contact-form").slideUp(800);
+                $('#answer').html(html);
+            }
+        });
+
+        // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепочку срабатывания остальных функций
+        return false;
+    }
+
+
+});
